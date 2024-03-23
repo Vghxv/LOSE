906cd0cd92c74125264fa45971e533cb155b5ed2
Vincent <hankggg2003xd@gmail.com>
2024-03-23 14:59:40 +0800

    refactor jiffies module

--- jiffies/jiffies_module.c
+++ jiffies/jiffies_module.c
@@ -3,59 +3,99 @@
 #include <linux/module.h>
 #include <linux/uaccess.h>
 #include <linux/jiffies.h>
+#include <linux/seq_file.h>
 
-#define PROC_FILENAME "jiffies"
-#define BUFFER_SIZE 128
+#define PROC_JIFFIES "jiffies"
+#define PROC_SECS "seconds"
 
-static struct proc_dir_entry *proc_entry;
+static struct proc_dir_entry *proc_entry_jiffies;
+static struct proc_dir_entry *proc_entry_secs;
+static unsigned long start_jiffies;
 
-static ssize_t jiffies_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
+// this function logs the current value of jiffies to the kernel log
+static int print_jiffies(struct seq_file *m, void *v)
 {
-    char temp[BUFFER_SIZE];
-    ssize_t len;
-    
-    len = snprintf(temp, sizeof(temp), "%lu\n", jiffies);
-    
-    if (*pos >= len)
-        return 0;
-    
-    if (count > len - *pos)
-        count = len - *pos;
-    
-    if (copy_to_user(buf, temp + *pos, count))
-        return -EFAULT;
-    
-    *pos += count;
-    
-    return count;
+    seq_printf(m, "current value of jiffies = %lu\n", jiffies);
+    return 0;
 }
 
-static const struct proc_ops jiffies_fops = {
-    .proc_read = jiffies_read,
-};
-
-
-int init_module()
+// this function is called when the /proc/jiffies file is read
+static int jiffies_open(struct inode *inode, struct file *file)
 {
-    proc_entry = proc_create(PROC_FILENAME, 0, NULL, &jiffies_fops);
-    if (!proc_entry) {
-        printk(KERN_ERR "Failed to create /proc/%s\n", PROC_FILENAME);
+    return single_open(file, print_jiffies, NULL);
+}
+
+
+static int print_seconds(struct seq_file *m, void *v)
+{
+    seq_printf(m, "current value of seconds since the module was loaded = %lu\n", (jiffies - start_jiffies) / HZ);
+    return 0;
+}
+
+// this function is called when the /proc/seconds file is read
+static int seconds_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, print_seconds, NULL);
+}
+
+// __init macro tells the kernel that this function is only used at initialization time
+static int __init custom_module_init(void) 
+{
+    // put it inside the init_module function to limit the scope of the struct proc_ops  
+    static const struct proc_ops jiffies_fops = {
+        .proc_open = jiffies_open,
+        .proc_read = seq_read,
+        .proc_lseek = seq_lseek,
+        .proc_release = single_release,
+    };
+
+    static const struct proc_ops seconds_fops = {
+        .proc_open = seconds_open,
+        .proc_read = seq_read,
+        .proc_lseek = seq_lseek,
+        .proc_release = single_release,
+    };
+
+    start_jiffies = jiffies;
+    proc_entry_jiffies = proc_create(PROC_JIFFIES, 0, NULL, &jiffies_fops);
+    if (!proc_entry_jiffies) {
+        printk(KERN_ERR "Failed to create /proc/%s\n", PROC_JIFFIES);
         return -ENOMEM;
     }
+    printk(KERN_INFO "/proc/%s created\n", PROC_JIFFIES);
 
-    printk(KERN_INFO "/proc/%s created\n", PROC_FILENAME);
+    proc_entry_secs = proc_create(PROC_SECS, 0, NULL, &seconds_fops);
+    if (!proc_entry_secs) {
+        printk(KERN_ERR "Failed to create /proc/%s\n", PROC_SECS);
+        return -ENOMEM;
+    }
+    printk(KERN_INFO "/proc/%s created\n", PROC_SECS);
     
     return 0;
 }
 
-void cleanup_module()
+// __exit macro tells the kernel that this function is only used at exit time
+static void __exit custom_module_exit(void) 
 {
-    if (proc_entry) {
-        remove_proc_entry(PROC_FILENAME, NULL);
-        printk(KERN_INFO "/proc/%s removed\n", PROC_FILENAME);
+    if (proc_entry_jiffies) {
+        remove_proc_entry(PROC_JIFFIES, NULL);
+        printk(KERN_INFO "/proc/%s removed\n", PROC_JIFFIES);
+    }
+
+    if (proc_entry_secs) {
+        remove_proc_entry(PROC_SECS, NULL);
+        printk(KERN_INFO "/proc/%s removed\n", PROC_SECS);
     }
 }
 
+module_init(custom_module_init);
+module_exit(custom_module_exit);
+
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Report the current value of jiffies when the /proc/jiffies file is read.");
-MODULE_AUTHOR("LOSE");
+MODULE_DESCRIPTION("
+    Jiffies Module
+    This module creates two files in /proc:
+    - /proc/jiffies: prints the current value of jiffies
+    - /proc/seconds: prints the number of seconds since the module was loaded
+");
+MODULE_AUTHOR("vincent");
