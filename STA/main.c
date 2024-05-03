814fed826943978b2d5e32195821346ac0f07e53
Vincent <hankggg2003xd@gmail.com>
2024-05-04 01:00:00 +0800

    student wait TA finish teaching and add kokoko sound

--- STA/main.c
+++ STA/main.c
@@ -1,18 +1,17 @@
-#include <complex.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <semaphore.h>
 
-#define CHAIR_NUM 4
+#define CHAIR_NUM 3
 #define STUDENT_NUM 5
-sem_t cond;
-// pthread_cond_t ta_finish_cond;
-pthread_mutex_t mutex;
-pthread_mutex_t list_mutex;
-sem_t sem;
-sem_t sem_ta_finish;
+
+pthread_mutex_t queue_mutex;
+sem_t ta_sleeping;
+sem_t chairs;
+sem_t ta_finish;
 
 typedef struct student_queue{
     pthread_t student_thread;
@@ -21,20 +20,20 @@ typedef struct student_queue{
 student_queue_t *head = NULL;
 
 void pop(){
-    pthread_mutex_lock(&list_mutex);
+    pthread_mutex_lock(&queue_mutex);
     student_queue_t *temp = head->next;
     if (temp == NULL){
-        pthread_mutex_unlock(&list_mutex);
+        pthread_mutex_unlock(&queue_mutex);
         exit(1);
     }
     head->next = temp->next;
     pthread_t student_thread = temp->student_thread;
     free(temp);
-    pthread_mutex_unlock(&list_mutex);
+    pthread_mutex_unlock(&queue_mutex);
 }
 
 void push(pthread_t student_thread){
-    pthread_mutex_lock(&list_mutex);
+    pthread_mutex_lock(&queue_mutex);
     student_queue_t *temp = head;
     while(temp->next != NULL){
         temp = temp->next;
@@ -43,59 +42,59 @@ void push(pthread_t student_thread){
     new_node->student_thread = student_thread;
     new_node->next = NULL;
     temp->next = new_node;
-    pthread_mutex_unlock(&list_mutex);
+    pthread_mutex_unlock(&queue_mutex);
 }
 
 void *teaching_assistant(void *arg){
     while(1){
-        printf("Teaching assistant is taking a nap\n");
-        // pthread_cond_wait(&cond, &mutex);
-        sem_wait(&cond);
-
-        printf("Teaching assistant is helping student\n");
+        pthread_mutex_lock(&queue_mutex);
+        if (head->next == NULL){
+            pthread_mutex_unlock(&queue_mutex);
+            printf("TA is sleeping\n");
+            sem_wait(&ta_sleeping);
+        }
+        else{
+            pthread_t student_thread = head->next->student_thread;
+            pthread_mutex_unlock(&queue_mutex);
+            printf("TA calls student %ld\n", student_thread);
+            sem_post(&ta_finish);
+        }
+        printf("TA is helping student\n");
         sleep(1);
-        printf("Teaching assistant finish helping student\n");
-        sem_post(&sem_ta_finish);
+        printf("TA finish helping student\n");
     }
 }
 
 void *student(void *arg){
     int wait_time;
     while(1){
-        sleep(1);
+        wait_time = rand() % 5 + 5;
+        sleep(wait_time);
         printf("Student %ld is coming\n", pthread_self());
-
-        sem_wait(&sem);
+        sem_wait(&chairs);
         push(pthread_self());
-        printf("Student %ld is sitting on the chair\n", pthread_self());
-
+        int chairs_n;
+        sem_getvalue(&chairs, &chairs_n);
+        printf("Student %ld is sitting on the chair, chair left %d\n", pthread_self(), chairs_n);
         while(head->next->student_thread != pthread_self());
-
-        pthread_mutex_lock(&mutex);
         printf("kokoko🚪🤛, Student %ld is smashing the door\n", pthread_self());
-        // play sfx of knocking the door from a file named "knock.mp3"
-        // system("mpg123 knock.mp3");
-
-        
-        // pthread_cond_signal(&cond);
-        sem_post(&cond);
+        system("play -q knock.mp3 -t alsa");
+ 
+        sem_post(&ta_sleeping);
+        sem_wait(&ta_finish);
         pop();
-
-        sem_post(&sem);
-        sem_wait(&sem_ta_finish);
-        printf("Student %ld is leaving\n", pthread_self());
-        pthread_mutex_unlock(&mutex); 
+        sem_post(&chairs);
+        printf("Student %ld stand up and leave\n", pthread_self());
     }
 }
 
 int main(int argc, char *argv[]){
     pthread_t ta;
     pthread_t students[STUDENT_NUM];
-    sem_init(&sem, 0, CHAIR_NUM);
-    sem_init(&sem_ta_finish, 0, 1);
-    sem_init(&cond, 0, 1);
-    pthread_mutex_init(&mutex, NULL);
-    pthread_mutex_init(&list_mutex, NULL);
+    sem_init(&chairs, 0, CHAIR_NUM);
+    sem_init(&ta_finish, 0, 1);
+    sem_init(&ta_sleeping, 0, 1);
+    pthread_mutex_init(&queue_mutex, NULL);
     head = (student_queue_t *)malloc(sizeof(student_queue_t));
     pthread_create(&ta, NULL, teaching_assistant, NULL);
     for(int i = 0; i < STUDENT_NUM; i++){
